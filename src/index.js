0ba9da0f185c66774f4197df65eea70b46df9af9
yamafaktory <davy@qipp.com>
2016-05-25 17:14:57 +0200

    Removed unecessary dependency as Bublé now supports JSX.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { DummyComponent } from './components/dummy-component.jsx'
 
-// Define the root element and instantiate the DummyComponent.
+// Define the root element.
 const root = document.querySelector('main')
-const dummyComponent = React.createElement(DummyComponent)
 
 // Append the DummyComponent instance to the root element.
-ReactDOM.render(dummyComponent, root)
+ReactDOM.render(<DummyComponent />, root)
